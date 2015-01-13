a86113a05f72f0d1fd072741d95313b8ba3c743f
Byron Knoll <byron@byronknoll.com>
2015-01-13 00:08:44 -0800

    jpeg detection

--- src/preprocess/preprocessor.cpp
+++ src/preprocess/preprocessor.cpp
@@ -78,7 +78,7 @@ Filetype detect(FILE* in, int n, Filetype type) {
   int e8e9last=0;
 
   // For JPEG detection
-  int soi=0, sof=0, sos=0;  // position where found
+  int soi=0, sof=0, sos=0, app=0;
 
   // For TEXT detection
   int ascii_start = -1;
@@ -91,18 +91,19 @@ Filetype detect(FILE* in, int n, Filetype type) {
     buf1=buf1<<8|buf0>>24;
     buf0=buf0<<8|c;
 
-    if (i>=3 && (buf0&0xfffffff0)==0xffd8ffe0) soi=i;
-    if (soi && i-soi<0x10000 && (buf1&0xff)==0xff
-        && (buf0&0xff0000ff)==0xc0000008)
-      sof=i;
-    if (soi && sof && sof>soi && i-soi<0x10000 && i-sof<0x1000
-        && (buf0&0xffff)==0xffda) {
-      sos=i;
-      if (type!=JPEG) return fseek(in, start+soi-3, SEEK_SET), JPEG;
+    if (!soi && i>=3 && (buf0&0xfffffff0)==0xffd8ffe0) soi=i, app=i+2, sos=sof=0;
+    if (soi) {
+      if (app==i && (buf0>>24)==0xff &&
+         ((buf0>>16)&0xff)>0xc0 && ((buf0>>16)&0xff)<0xff) app=i+(buf0&0xffff)+2;
+      if (app<i && (buf1&0xff)==0xff && (buf0&0xff0000ff)==0xc0000008) sof=i;
+      if (sof && sof>soi && i-sof<0x1000 && (buf0&0xffff)==0xffda) {
+        sos=i;
+        if (type!=JPEG) return fseek(in, start+soi-3, SEEK_SET), JPEG;
+      }
+      if (i-soi>0x40000 && !sos) soi=0;
     }
     if (type==JPEG && sos && i>sos && (buf0&0xff00)==0xff00
-        && (buf0&0xff)!=0 && (buf0&0xf8)!=0xd0)
-      return DEFAULT;
+        && (buf0&0xff)!=0 && (buf0&0xf8)!=0xd0) return DEFAULT;
 
     if (((buf1&0xfe)==0xe8 || (buf1&0xfff0)==0x0f80) && ((buf0+1)&0xfe)==0) {
       int r=buf0>>24;
