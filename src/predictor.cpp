53cc21807aa0d617e511719a3f24ff09cbf8ac68
Byron Knoll <byron@byronknoll.com>
2017-03-08 14:32:02 -0800

    skip connection for byte mixer

--- src/predictor.cpp
+++ src/predictor.cpp
@@ -305,6 +305,7 @@ void Predictor::AddSSE() {
 void Predictor::AddMixers() {
   byte_mixer_.reset(new ByteMixer(256 * byte_models_.size(), 60, 2, 40, 0.05,
       manager_.bit_context_));
+  auxiliary_.push_back(models_.size() + byte_models_.size());
 
   for (int i = 0; i < 3; ++i) {
     layers_.push_back(std::unique_ptr<MixerInput>(new MixerInput(logistic_,
