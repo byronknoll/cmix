2d10324297b30f4f6181b84a7f9d6968bdf82357
Márcio Pais <MarcioPais@users.noreply.github.com>
2017-12-24 18:53:07 +0100

    Add AUDIO FileType

--- src/preprocess/preprocessor.h
+++ src/preprocess/preprocessor.h
@@ -9,7 +9,7 @@
 namespace preprocessor {
 
 typedef enum {DEFAULT, HDR, JPEG, EXE, TEXT, IMAGE1, IMAGE4, IMAGE8, IMAGE8GRAY,
-    IMAGE24, IMAGE32} Filetype;
+    IMAGE24, IMAGE32, AUDIO} Filetype;
 
 inline bool HasInfo(Filetype ft) { return ft==TEXT || ft==IMAGE1 || ft==IMAGE4
     || ft==IMAGE8 || ft==IMAGE8GRAY || ft==IMAGE24 || ft==IMAGE32; }
