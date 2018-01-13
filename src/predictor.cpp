38a1c2838c2486dca7434d97124303de64113927
Byron <byron@byronknoll.com>
2018-01-13 09:40:54 -0800

    reduce LSTM size

--- src/predictor.cpp
+++ src/predictor.cpp
@@ -269,7 +269,7 @@ void Predictor::AddMixers() {
   for (unsigned int i = 0; i < vocab_.size(); ++i) {
     if (vocab_[i]) ++vocab_size;
   }
-  AddByteMixer(new ByteMixer(byte_models_.size(), 200, 2, 100, 0.05, 10,
+  AddByteMixer(new ByteMixer(byte_models_.size(), 150, 2, 40, 0.03, 10,
       manager_.bit_context_, vocab_, vocab_size));
   auxiliary_.push_back(models_.size() + byte_models_.size());
 
