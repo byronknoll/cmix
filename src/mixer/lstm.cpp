b0195468201ca8c2c07f4540b5ffa45206e6abcf
Byron Knoll <byron@byronknoll.com>
2018-01-19 18:00:24 -0800

    lstm random seed

--- src/mixer/lstm.cpp
+++ src/mixer/lstm.cpp
@@ -14,7 +14,7 @@ Lstm::Lstm(unsigned int input_size, unsigned int output_size, unsigned int
     output_(std::valarray<float>(1.0 / output_size, output_size), horizon),
     learning_rate_(learning_rate), num_cells_(num_cells), epoch_(0),
     horizon_(horizon), input_size_(input_size), output_size_(output_size) {
-  srand(0x55555);
+  srand(14003);
   hidden_[hidden_.size() - 1] = 1;
   for (int epoch = 0; epoch < horizon; ++epoch) {
     layer_input_[epoch][0].resize(1 + num_cells + input_size);
