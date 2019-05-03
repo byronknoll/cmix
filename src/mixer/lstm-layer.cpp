83c1382407bbb10df0ae61203274f896b7273b8d
Byron Knoll <byron@byronknoll.com>
2019-05-02 18:19:05 -0700

    refactoring

--- src/mixer/lstm-layer.cpp
+++ src/mixer/lstm-layer.cpp
@@ -25,40 +25,26 @@ void Adam(std::valarray<float>* g, std::valarray<float>* m,
 
 LstmLayer::LstmLayer(unsigned int input_size, unsigned int auxiliary_input_size,
     unsigned int output_size, unsigned int num_cells, int horizon,
-    float gradient_clip, float learning_rate) : state_(num_cells),
-    output_gate_error_(num_cells), state_error_(num_cells),
-    input_node_error_(num_cells), forget_gate_error_(num_cells),
-    stored_error_(num_cells),
+    float gradient_clip, float learning_rate) :
+    forget_gate_(input_size, num_cells, horizon),
+    input_node_(input_size, num_cells, horizon),
+    output_gate_(input_size, num_cells, horizon),
+    state_(num_cells), state_error_(num_cells), stored_error_(num_cells),
     tanh_state_(std::valarray<float>(num_cells), horizon),
-    output_gate_state_(std::valarray<float>(num_cells), horizon),
-    input_node_state_(std::valarray<float>(num_cells), horizon),
     input_gate_state_(std::valarray<float>(num_cells), horizon),
-    forget_gate_state_(std::valarray<float>(num_cells), horizon),
     last_state_(std::valarray<float>(num_cells), horizon),
-    forget_gate_(std::valarray<float>(input_size), num_cells),
-    input_node_(std::valarray<float>(input_size), num_cells),
-    output_gate_(std::valarray<float>(input_size), num_cells),
-    forget_gate_update_(std::valarray<float>(input_size), num_cells),
-    input_node_update_(std::valarray<float>(input_size), num_cells),
-    output_gate_update_(std::valarray<float>(input_size), num_cells),
-    forget_gate_m_(std::valarray<float>(input_size), num_cells),
-    input_node_m_(std::valarray<float>(input_size), num_cells),
-    output_gate_m_(std::valarray<float>(input_size), num_cells),
-    forget_gate_v_(std::valarray<float>(input_size), num_cells),
-    input_node_v_(std::valarray<float>(input_size), num_cells),
-    output_gate_v_(std::valarray<float>(input_size), num_cells),
     gradient_clip_(gradient_clip), learning_rate_(learning_rate),
     num_cells_(num_cells), epoch_(0), horizon_(horizon),
     input_size_(auxiliary_input_size), output_size_(output_size) {
   float low = -0.2;
   float range = 0.4;
   for (unsigned int i = 0; i < num_cells_; ++i) {
-    for (unsigned int j = 0; j < forget_gate_[i].size(); ++j) {
-      forget_gate_[i][j] = low + Rand() * range;
-      input_node_[i][j] = low + Rand() * range;
-      output_gate_[i][j] = low + Rand() * range;
+    for (unsigned int j = 0; j < forget_gate_.weights_[i].size(); ++j) {
+      forget_gate_.weights_[i][j] = low + Rand() * range;
+      input_node_.weights_[i][j] = low + Rand() * range;
+      output_gate_.weights_[i][j] = low + Rand() * range;
     }
-    forget_gate_[i][forget_gate_[i].size() - 1] = 1;
+    forget_gate_.weights_[i][forget_gate_.weights_[i].size() - 1] = 1;
   }
 }
 
@@ -66,22 +52,24 @@ void LstmLayer::ForwardPass(const std::valarray<float>& input, int input_symbol,
     std::valarray<float>* hidden, int hidden_start) {
   last_state_[epoch_] = state_;
   for (unsigned int i = 0; i < num_cells_; ++i) {
-    forget_gate_state_[epoch_][i] = Sigmoid::Logistic(std::inner_product(
+    forget_gate_.state_[epoch_][i] = Sigmoid::Logistic(std::inner_product(
         &input[0], &input[input.size()],
-        &forget_gate_[i][output_size_], forget_gate_[i][input_symbol]));
-    input_node_state_[epoch_][i] = tanh(std::inner_product(&input[0],
-        &input[input.size()], &input_node_[i][output_size_],
-        input_node_[i][input_symbol]));
-    output_gate_state_[epoch_][i] = Sigmoid::Logistic(std::inner_product(
+        &forget_gate_.weights_[i][output_size_],
+        forget_gate_.weights_[i][input_symbol]));
+    input_node_.state_[epoch_][i] = tanh(std::inner_product(&input[0],
+        &input[input.size()], &input_node_.weights_[i][output_size_],
+        input_node_.weights_[i][input_symbol]));
+    output_gate_.state_[epoch_][i] = Sigmoid::Logistic(std::inner_product(
         &input[0], &input[input.size()],
-        &output_gate_[i][output_size_], output_gate_[i][input_symbol]));
+        &output_gate_.weights_[i][output_size_],
+        output_gate_.weights_[i][input_symbol]));
   }
-  input_gate_state_[epoch_] = 1.0f - forget_gate_state_[epoch_];
-  state_ *= forget_gate_state_[epoch_];
-  state_ += input_node_state_[epoch_] * input_gate_state_[epoch_];
+  input_gate_state_[epoch_] = 1.0f - forget_gate_.state_[epoch_];
+  state_ *= forget_gate_.state_[epoch_];
+  state_ += input_node_.state_[epoch_] * input_gate_state_[epoch_];
   tanh_state_[epoch_] = tanh(state_);
   std::slice slice = std::slice(hidden_start, num_cells_, 1);
-  (*hidden)[slice] = output_gate_state_[epoch_] * tanh_state_[epoch_];
+  (*hidden)[slice] = output_gate_.state_[epoch_] * tanh_state_[epoch_];
   ++epoch_;
   if (epoch_ == horizon_) epoch_ = 0;
 }
@@ -99,44 +87,50 @@ void LstmLayer::BackwardPass(const std::valarray<float>&input, int epoch,
     stored_error_ = *hidden_error;
     state_error_ = 0;
     for (unsigned int i = 0; i < num_cells_; ++i) {
-      forget_gate_update_[i] = 0;
-      input_node_update_[i] = 0;
-      output_gate_update_[i] = 0;
+      forget_gate_.update_[i] = 0;
+      input_node_.update_[i] = 0;
+      output_gate_.update_[i] = 0;
     }
   } else {
     stored_error_ += *hidden_error;
   }
 
-  output_gate_error_ = tanh_state_[epoch] * stored_error_ *
-      output_gate_state_[epoch] * (1.0f - output_gate_state_[epoch]);
-  state_error_ += stored_error_ * output_gate_state_[epoch] * (1.0f -
+  output_gate_.error_ = tanh_state_[epoch] * stored_error_ *
+      output_gate_.state_[epoch] * (1.0f - output_gate_.state_[epoch]);
+  state_error_ += stored_error_ * output_gate_.state_[epoch] * (1.0f -
       (tanh_state_[epoch] * tanh_state_[epoch]));
-  input_node_error_ = state_error_ * input_gate_state_[epoch] * (1.0f -
-      (input_node_state_[epoch] * input_node_state_[epoch]));
-  forget_gate_error_ = (last_state_[epoch] - input_node_state_[epoch]) *
-      state_error_ * forget_gate_state_[epoch] * input_gate_state_[epoch];
+  input_node_.error_ = state_error_ * input_gate_state_[epoch] * (1.0f -
+      (input_node_.state_[epoch] * input_node_.state_[epoch]));
+  forget_gate_.error_ = (last_state_[epoch] - input_node_.state_[epoch]) *
+      state_error_ * forget_gate_.state_[epoch] * input_gate_state_[epoch];
 
   *hidden_error = 0;
   if (layer > 0) {
     int offset = output_size_ + num_cells_ + input_size_;
     for (unsigned int i = 0; i < num_cells_; ++i) {
       for (unsigned int j = offset; j < offset + num_cells_; ++j) {
-        (*hidden_error)[j-offset] += input_node_[i][j] * input_node_error_[i];
-        (*hidden_error)[j-offset] += forget_gate_[i][j] * forget_gate_error_[i];
-        (*hidden_error)[j-offset] += output_gate_[i][j] * output_gate_error_[i];
+        (*hidden_error)[j-offset] += input_node_.weights_[i][j] *
+            input_node_.error_[i];
+        (*hidden_error)[j-offset] += forget_gate_.weights_[i][j] *
+            forget_gate_.error_[i];
+        (*hidden_error)[j-offset] += output_gate_.weights_[i][j] *
+            output_gate_.error_[i];
       }
     }
   }
 
   if (epoch > 0) {
-    state_error_ *= forget_gate_state_[epoch];
+    state_error_ *= forget_gate_.state_[epoch];
     stored_error_ = 0;
     int offset = output_size_ + input_size_;
     for (unsigned int i = 0; i < num_cells_; ++i) {
       for (unsigned int j = offset; j < offset + num_cells_; ++j) {
-        stored_error_[j-offset] += input_node_[i][j] * input_node_error_[i];
-        stored_error_[j-offset] += forget_gate_[i][j] * forget_gate_error_[i];
-        stored_error_[j-offset] += output_gate_[i][j] * output_gate_error_[i];
+        stored_error_[j-offset] += input_node_.weights_[i][j] *
+            input_node_.error_[i];
+        stored_error_[j-offset] += forget_gate_.weights_[i][j] *
+            forget_gate_.error_[i];
+        stored_error_[j-offset] += output_gate_.weights_[i][j] *
+            output_gate_.error_[i];
       }
     }
   }
@@ -147,22 +141,22 @@ void LstmLayer::BackwardPass(const std::valarray<float>&input, int epoch,
 
   std::slice slice = std::slice(output_size_, input.size(), 1);
   for (unsigned int i = 0; i < num_cells_; ++i) {
-    forget_gate_update_[i][slice] += forget_gate_error_[i] * input;
-    input_node_update_[i][slice] += input_node_error_[i] * input;
-    output_gate_update_[i][slice] += output_gate_error_[i] * input;
-    forget_gate_update_[i][input_symbol] += forget_gate_error_[i];
-    input_node_update_[i][input_symbol] += input_node_error_[i];
-    output_gate_update_[i][input_symbol] += output_gate_error_[i];
+    forget_gate_.update_[i][slice] += forget_gate_.error_[i] * input;
+    input_node_.update_[i][slice] += input_node_.error_[i] * input;
+    output_gate_.update_[i][slice] += output_gate_.error_[i] * input;
+    forget_gate_.update_[i][input_symbol] += forget_gate_.error_[i];
+    input_node_.update_[i][input_symbol] += input_node_.error_[i];
+    output_gate_.update_[i][input_symbol] += output_gate_.error_[i];
   }
   if (epoch == 0) {
     ++update_steps_;
     for (unsigned int i = 0; i < num_cells_; ++i) {
-      Adam(&forget_gate_update_[i], &forget_gate_m_[i], &forget_gate_v_[i],
-          &forget_gate_[i], learning_rate_, update_steps_);
-      Adam(&input_node_update_[i], &input_node_m_[i], &input_node_v_[i],
-          &input_node_[i], learning_rate_, update_steps_);
-      Adam(&output_gate_update_[i], &output_gate_m_[i], &output_gate_v_[i],
-          &output_gate_[i], learning_rate_, update_steps_);
+      Adam(&forget_gate_.update_[i], &forget_gate_.m_[i], &forget_gate_.v_[i],
+          &forget_gate_.weights_[i], learning_rate_, update_steps_);
+      Adam(&input_node_.update_[i], &input_node_.m_[i], &input_node_.v_[i],
+          &input_node_.weights_[i], learning_rate_, update_steps_);
+      Adam(&output_gate_.update_[i], &output_gate_.m_[i], &output_gate_.v_[i],
+          &output_gate_.weights_[i], learning_rate_, update_steps_);
     }
   }
 }
--- src/mixer/lstm-layer.h
+++ src/mixer/lstm-layer.h
@@ -5,6 +5,18 @@
 #include <stdlib.h>
 #include <math.h>
 
+struct NeuronLayer {
+  NeuronLayer(unsigned int input_size, unsigned int num_cells, int horizon) :
+    error_(num_cells), weights_(std::valarray<float>(input_size), num_cells),
+    state_(std::valarray<float>(num_cells), horizon),
+    update_(std::valarray<float>(input_size), num_cells),
+    m_(std::valarray<float>(input_size), num_cells),
+    v_(std::valarray<float>(input_size), num_cells) {};
+
+  std::valarray<float> error_;
+  std::valarray<std::valarray<float>> weights_, state_, update_, m_, v_;
+};
+
 class LstmLayer {
  public:
   LstmLayer(unsigned int input_size, unsigned int auxiliary_input_size,
@@ -19,13 +31,10 @@ class LstmLayer {
   }
 
  private:
-  std::valarray<float> state_, output_gate_error_, state_error_,
-      input_node_error_, forget_gate_error_, stored_error_;
-  std::valarray<std::valarray<float>> tanh_state_, output_gate_state_,
-      input_node_state_, input_gate_state_, forget_gate_state_, last_state_,
-      forget_gate_, input_node_, output_gate_, forget_gate_update_,
-      input_node_update_, output_gate_update_, forget_gate_m_, input_node_m_,
-      output_gate_m_, forget_gate_v_, input_node_v_, output_gate_v_;
+  NeuronLayer forget_gate_, input_node_, output_gate_;
+  std::valarray<float> state_, state_error_, stored_error_;
+  std::valarray<std::valarray<float>> tanh_state_, input_gate_state_,
+      last_state_;
   float gradient_clip_, learning_rate_;
   unsigned int num_cells_, epoch_, horizon_, input_size_, output_size_;
   unsigned long long update_steps_ = 0;
